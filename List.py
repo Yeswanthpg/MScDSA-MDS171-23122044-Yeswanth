93b74e63a4df98f8cc2080f3fe5faca90ae00ee1
Yeswanthpg <yeswanthpg@gmail.com>
2023-08-25 09:13:38 +0530

    python commands

---
+++ List.py
@@ -0,0 +1,4 @@
+listplace  = ["Item 1", "Item 2",10,22.45]
+#type(listplace) --> gives the datatype of the variable 
+print(listplace[3])
+print(type(listplace))
---
+++ String length.py
@@ -0,0 +1,6 @@
+msg = " This is a string"
+print(msg)
+print(len(msg))
+msg = msg.strip()
+print(len(msg))
+print(msg)
